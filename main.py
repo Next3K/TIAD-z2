17be5a5498dbd7ba0423ccf223c12f84d0cb09dd
Pawel <236691@edu.p.lodz.pl>
2023-04-25 18:01:46 +0200

    added loggs

--- main.py
+++ main.py
@@ -29,38 +29,42 @@ if __name__ == '__main__':
     cognitive_constant: float = 1.2
 
     # conduct experiments for all functions
-    for function in functions:
-        for number_of_dimensions in dimensions:
-            # set proper number od dimensions
-            function.dimensions = number_of_dimensions
+    for num_of_swarms in [4, 5, 8]:
+        print(f"\n\n\n CURRENT NUMBER OF SWARMS IS {num_of_swarms} \n\n\n")
+        for function in functions:
+            for number_of_dimensions in dimensions:
+                print(f"\n\n\n CURRENT NUMBER OF DIMENSIONS IS {number_of_dimensions} \n\n\n")
 
-            conductor_opso = Conductor(30,
-                                       algorithm_type="osmosis",
-                                       function=function,
-                                       stop_criterion=stop_criterion_iterations,
-                                       number_of_swarms=number_of_swarms,
-                                       swarm_size=swarm_size)
+                # set proper number od dimensions
+                function.dimensions = number_of_dimensions
 
-            conductor_epso = Conductor(30,
-                                       algorithm_type="elite",
-                                       function=function,
-                                       stop_criterion=stop_criterion_iterations,
-                                       number_of_swarms=number_of_swarms,
-                                       swarm_size=swarm_size)
+                conductor_opso = Conductor(30,
+                                           algorithm_type="osmosis",
+                                           function=function,
+                                           stop_criterion=stop_criterion_iterations,
+                                           number_of_swarms=number_of_swarms,
+                                           swarm_size=swarm_size)
 
-            print(f"EPSO algorithm results for function {function.__class__} (dimensions: {number_of_dimensions}):")
-            print(
-                f"Best solution: {conductor_epso.best_solution},"
-                f" avg solution: {conductor_epso.average_solution},"
-                f" part success: {conductor_epso.part_successful},"
-                f" standard deviation: {conductor_epso.standard_deviation}")
+                conductor_epso = Conductor(30,
+                                           algorithm_type="elite",
+                                           function=function,
+                                           stop_criterion=stop_criterion_iterations,
+                                           number_of_swarms=number_of_swarms,
+                                           swarm_size=swarm_size)
 
-            print(f"OPSO algorithm results for function {function.__class__} (dimensions: {number_of_dimensions}):")
-            print(
-                f"Best solution: {conductor_opso.best_solution},"
-                f" avg solution: {conductor_opso.average_solution},"
-                f" part success: {conductor_opso.part_successful},"
-                f" standard deviation: {conductor_opso.standard_deviation}")
+                print(f"EPSO algorithm results for function {function.__class__} (dimensions: {number_of_dimensions}):")
+                print(
+                    f"Best solution: {conductor_epso.best_solution},"
+                    f" avg solution: {conductor_epso.average_solution},"
+                    f" part success: {conductor_epso.part_successful},"
+                    f" standard deviation: {conductor_epso.standard_deviation}")
 
-            print_chart(conductor_opso.trace_list, "OPSO")
-            print_chart(conductor_epso.trace_list, "EPSO")
+                print(f"OPSO algorithm results for function {function.__class__} (dimensions: {number_of_dimensions}):")
+                print(
+                    f"Best solution: {conductor_opso.best_solution},"
+                    f" avg solution: {conductor_opso.average_solution},"
+                    f" part success: {conductor_opso.part_successful},"
+                    f" standard deviation: {conductor_opso.standard_deviation}")
+
+                print_chart(conductor_opso.trace_list, "OPSO")
+                print_chart(conductor_epso.trace_list, "EPSO")
